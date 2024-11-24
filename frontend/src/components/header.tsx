8868796b3fe69aaa4f1ca0d8e5dd6ed1d8bfcf72
Isadora <dorisperdigao@gmail.com>
2024-11-24 12:16:42 -0300

    styles header

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -1,3 +1,12 @@
+import Image from "next/image";
+import car_icon from "../assets/directions_car.svg"
+
 export default function Header () {
-    return <header>Hello world!</header>
+    return (
+        <header className="flex justify-between p-5 items-center bg-gray-900 shadow-lg">
+            <span className="text-green-700 text-xs italic">SHOPPER RIDES</span>
+            <h1 className="text-green-600 font-bold text-3xl">Shopper Rides</h1>
+            <Image src={car_icon} width={30} height={30} color="#15803d" alt="Car Icon"/> 
+        </header>
+    )
 }
