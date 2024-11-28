e7f135641caf37c22bdb43c0a09015fa1a442051
IsadoraPerdigao <dorisperdigao@gmail.com>
2024-11-27 23:09:40 -0300

    add redirection

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,13 +1,18 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-
-};
-
-module.exports = {
   reactStrictMode: true, // Optional: enable React strict mode
   images: {
     domains: ['maps.googleapis.com'], // Allow images from Google Maps API
+  },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/ride/estimate',
+        permanent: true,
+      },
+    ];
   },
 };
 
